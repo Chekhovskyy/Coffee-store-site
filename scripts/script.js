fa599eaec3fba7e55d922955515ed1e2649ee8d5
Chekhovskyy <pashachekh@gmail.com.com>
2025-03-14 18:29:20 +0200

    js-new-script

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ burgerButton.addEventListener('click', function () {
     const nav = document.querySelector('.nav');
     nav.classList.toggle('active-burger');
 });
+
+const menuLinks = document.querySelectorAll('.burger-menu .list-nav .item-nav a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        if (this.getAttribute('href').includes('#') && this.getAttribute('href') !== '#') {
+            const nav = document.querySelector('.nav');
+            if (nav.classList.contains('active-burger')) {
+                nav.classList.remove('active-burger');
+            }
+        }
+    });
+});
